05d6fd37190df6380cf9b90b98ed28e747047190
丁勇 <dingyong@dingyongdeMacBook-Pro.local>
2023-05-24 20:45:01 +0800

    init

---
+++ chat-api-interfaces/src/main/java/cn/dy/chatapi/ApiApplication.java
@@ -0,0 +1,18 @@
+package cn.dy.chatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: chat-gpt
+ * @description: 启动类
+ * @author: Bernard ding
+ * @create: 2023-05-23 23:49
+ **/
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ chat-api-interfaces/test/java/ApiTest.java
@@ -0,0 +1,24 @@
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @program: chat-gpt
+ * @description: 测试类
+ * @author: Bernard ding
+ * @create: 2023-05-23 23:55
+ **/
+@SpringBootTest
+public class ApiTest {
+
+
+    @Test
+    public void query_unanswered_questions() {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet httpGet = new HttpGet();
+
+    }
+}
